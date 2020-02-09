49e7f4fcde965ac2e212c1efa77ed0ba48d13e5d
Wojtek <ciubawojciech@gmail.com>
2020-02-09 23:22:05 +0100

    Sorting almost finished

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col, FormGroup, Label, Input } from "reactstrap";
 import "./assets/customStyles.css";
 
+import { GoTriangleUp, GoTriangleDown } from "react-icons/go";
+
 export default class App extends Component {
   state = {
     categories: ["fashion", "sports"], //can be extended by more categories
     activeCategories: [0, 1], //both categories selected as default
     articles: [],
+    sortDesc: true,
     error: null
   };
 
@@ -22,16 +25,67 @@ export default class App extends Component {
     this.clearErrors();
     this.clearArticles();
 
-    this.state.activeCategories.map((activeCat, index) => {
+    this.state.activeCategories.map(activeCat => {
       let activeCategoryName = this.state.categories[activeCat];
 
       getArticles(activeCategoryName).then(articles => {
-        articles.message
-          ? this.setState({ error: articles.message })
-          : this.setState({
-              articles: [...this.state.articles, ...articles.articles]
-            });
+        if (articles.message) {
+          this.setState({ error: articles.message });
+        } else {
+          let articlesArr = [...this.state.articles, ...articles.articles];
+          articlesArr = articlesArr.map(article => {
+            const dateSplitted = article.date.split(" ");
+            const monthNum = [
+              "januar",
+              "februar",
+              "mars",
+              "april",
+              "mai",
+              "juni",
+              "juli",
+              "august",
+              "september",
+              "oktober",
+              "november",
+              "desember"
+            ].indexOf(dateSplitted[1]); //Moment and internal parsing date library don't recognize oktober
+
+            const date = new Date(
+              dateSplitted[2],
+              monthNum,
+              dateSplitted[0].substring(0, dateSplitted[0].length - 1)
+            );
+
+            return {
+              ...article,
+              timeStamp: date
+            };
+          });
+          this.setState({
+            articles: articlesArr
+          });
+        }
       });
+    });
+  };
+
+  sortArticlesAsc() {
+    this.setState(prevState => {
+      this.state.articles.sort((a, b) => b.timeStamp - a.timeStamp);
+    });
+  }
+
+  sortArticlesDesc() {
+    this.setState(prevState => {
+      this.state.articles.sort((a, b) => a.timeStamp - b.timeStamp);
+    });
+  }
+
+  toggleSorting = () => {
+    this.setState({ sortDesc: !this.state.sortDesc }, () => {
+      this.state.sortDesc === true
+        ? this.sortArticlesDesc()
+        : this.sortArticlesAsc();
     });
   };
 
@@ -69,13 +123,21 @@ export default class App extends Component {
     return (
       <Container className="container" fluid>
         <Row>
-          <Col sm="12" xl="12">
-            {/* sort bar */}
+          <Col sm="12" xl="12" className="text-right">
+            <span onClick={this.toggleSorting}>
+              Sort by date:{" "}
+              {this.state.sortDesc === true ? (
+                <GoTriangleDown />
+              ) : (
+                <GoTriangleUp />
+              )}
+            </span>
           </Col>
         </Row>
         <Row>
           <Col sm="2">
-            <FormGroup check>
+            Categories:{" "}
+            <FormGroup check inline={window.innerWidth <= 760 ? true : false}>
               {this.state.categories.map((categoryName, index) => (
                 <div key={index}>
                   <Label check>
@@ -92,10 +154,12 @@ export default class App extends Component {
             </FormGroup>
           </Col>
           <Col sm="10">
-            {/* Error due to HTTP code 500, page reloaded*/}
+            {/* Error due to HTTP code 500, page reloaded. Can be resolved differently */}
             {this.state.error ? window.location.reload() : ""}
 
-            {this.state.activeCategories.length === 0 ? "Select at least one category!" : ""}
+            {this.state.activeCategories.length === 0
+              ? "Select at least one category!"
+              : ""}
             {this.state.articles.map((article, index) => (
               <div key={index}>
                 <Article article={article} />
--- src/components/Article.js
+++ src/components/Article.js
@@ -5,15 +5,15 @@ export default function Article(props) {
   const { id, date, image, category, title, preamble } = props.article;
 
   return (
-    <Media className="article">
-      <Media left href="#">
-        <img src={image} className="thumbnail"></img>
+    <Media>
+      <Media left>
+          {image ? <img src={image} className="thumbnail"></img> : <div className="thumbnail"><br />Thumbnail unavailable</div>}
       </Media>
-      <Media body>
+      <Media body className="articleBody">
         <Media heading>
           {title} <span className="date">{date}</span>
         </Media>
-        <small>{preamble}</small>
+        <span className="content"><small>{preamble}</small></span>
       </Media>
     </Media>
   );
