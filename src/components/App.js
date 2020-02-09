0805af4b1f058bee2d2145d4302a6b7fcc8ccac7
Wojtek <ciubawojciech@gmail.com>
2020-02-09 14:35:26 +0100

    Fetching raw data from API

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { getArticles } from "../services/API";
 
 export default class App extends Component {
-    render() {
-        return (
-            <div>
-                hello
-            </div>
-        )
-    }
+  state = {
+    categories: ["fashion", "sports"],
+    activeCategory: 0,
+    articles: []
+  };
+
+  componentDidMount() {
+    const activeCategoryName = this.state.categories[this.state.activeCategory];
+    getArticles(activeCategoryName).then(articles => this.setState(articles));
+  }
+
+  getCategoryNameCapitalized = categoryName => {
+    return categoryName.charAt(0).toUpperCase() + categoryName.slice(1);
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        {this.state.categories.map((categoryName, index) => (
+          <div key={index}>{this.getCategoryNameCapitalized(categoryName)}</div>
+        ))}
+        {this.state.articles.map((article, index) => (
+          <div key={index}>{article.id}</div>
+        ))}
+      </div>
+    );
+  }
 }
